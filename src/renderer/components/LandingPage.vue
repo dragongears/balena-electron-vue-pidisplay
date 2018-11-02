90469de5213201b1098405726eb85c6a28c622f8
Art Dahm <art@dahm.com>
2018-11-02 15:27:06 -0400

    Cooper weather

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -3,6 +3,15 @@
     <main>
       <div class="left-side">
         <system-information></system-information>
+        <weather
+            :update-interval="300"
+            api-key="1a3804ecac0cc9e49f0f47e5ea7cdf61"
+            title="Weather"
+            latitude="26.194221"
+            longitude="-80.1447177"
+            language="en"
+            units="us">
+        </weather>
       </div>
 
       <div class="right-side">
@@ -13,11 +22,16 @@
 </template>
 
 <script>
+  import VueWeatherWidget from 'vue-weather-widget'
+  import 'vue-weather-widget/dist/css/vue-weather-widget.css'
   import SystemInformation from './LandingPage/SystemInformation'
 
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    components: {
+      SystemInformation,
+      'weather': VueWeatherWidget
+    },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
@@ -45,22 +59,34 @@
         rgba(229, 229, 229, .9) 100%
       );
     height: 100vh;
-    padding: 36px 36px;
+    padding: 16px 16px;
     width: 100vw;
   }
 
   #cooper {
     height: auto;
-    margin-bottom: 20px;
-    width: 410px;
+    width: 260px;
+    margin-left: 30px;
+  }
+
+  .fe_alert {
+    display: none !important
+  }
+
+  .fe_container {
+    width: 480px;
+    font-weight: 300;
+    min-width: auto !important;
+    max-width: initial !important;
+    color: #333;
+    margin: 20px auto;
+    height: auto !important;
   }
 
   main {
     display: flex;
     justify-content: space-between;
   }
-
-  main > div { flex-basis: 50%; }
 
   .left-side {
     display: flex;
@@ -70,5 +96,6 @@
   .right-side {
     display: flex;
     flex-direction: column;
+    justify-content: flex-end;
   }
 </style>
