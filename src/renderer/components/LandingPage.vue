fe215de3e8e55353472df235e4a650b7ee8092e0
Art Dahm <art@dahm.com>
2018-11-02 11:38:31 -0400

    Cooper clock

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
       <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
         <system-information></system-information>
       </div>
 
@@ -53,16 +49,10 @@
     width: 100vw;
   }
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
-
   #cooper {
     height: auto;
     margin-bottom: 20px;
-    width: 256px;
+    width: 410px;
   }
 
   main {
@@ -77,46 +67,8 @@
     flex-direction: column;
   }
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    /*color: #2c3e50;*/
-    color: #e72250;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+  .right-side {
+    display: flex;
+    flex-direction: column;
   }
 </style>
--- src/renderer/components/LandingPage/SystemInformation.vue
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -1,30 +1,11 @@
 <template>
   <div>
-    <div class="title">Information</div>
     <div class="items">
       <div class="item">
-        <div class="name">Path:</div>
-        <div class="value">{{ path }}</div>
+        <div class="value">{{ dateStr }}</div>
       </div>
       <div class="item">
-        <div class="name">Route Name:</div>
-        <div class="value">{{ name }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Vue.js:</div>
-        <div class="value">{{ vue }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Electron:</div>
-        <div class="value">{{ electron }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Node:</div>
-        <div class="value">{{ node }}</div>
-      </div>
-      <div class="item">
-        <div class="name">Platform:</div>
-        <div class="value">{{ platform }}</div>
+        <div class="value">{{ timeStr }}</div>
       </div>
     </div>
   </div>
@@ -34,26 +15,31 @@
   export default {
     data () {
       return {
-        electron: process.versions['atom-shell'],
-        name: 'landing-page',
-        node: process.versions.node,
-        path: '/',
-        platform: require('os').platform(),
-        vue: require('vue/package.json').version
+        date: new Date()
       }
+    },
+    computed: {
+      dateStr: function () {
+        return this.date.toDateString()
+      },
+      timeStr: function () {
+        return this.date.toLocaleTimeString()
+      }
+    },
+    methods: {
+      intervalFunc () {
+        this.date = new Date()
+
+        setTimeout(this.intervalFunc, 1000)
+      }
+    },
+    mounted () {
+      this.intervalFunc()
     }
   }
 </script>
 
 <style scoped>
-  .title {
-    color: #888;
-    font-size: 18px;
-    font-weight: initial;
-    letter-spacing: .25px;
-    margin-top: 10px;
-  }
-
   .items { margin-top: 8px; }
 
   .item {
@@ -61,13 +47,9 @@
     margin-bottom: 6px;
   }
 
-  .item .name {
-    color: #6a6a6a;
-    margin-right: 6px;
-  }
-
   .item .value {
     color: #35495e;
+    font-size: 48px;
     font-weight: bold;
   }
 </style>
