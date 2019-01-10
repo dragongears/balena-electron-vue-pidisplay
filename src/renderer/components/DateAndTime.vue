9c7466ba3257422c162b6ec8c2978949f161b459
Art Dahm <art@dahm.com>
2019-01-10 10:41:26 -0500

    slot-scope test

--- src/renderer/components/DateAndTime.vue
+++ src/renderer/components/DateAndTime.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="items">
+    <div>TEST</div>
     <slot name="test" :teststr="timeStr"></slot>
     <div class="time">{{ timeStr }}</div>
     <div class="date">{{ dateStr }}</div>
